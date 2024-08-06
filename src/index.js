bd2a366b41aa118e7b5db69caef654aa0657d999
LIHAQ <57976650+LIHAQ@users.noreply.github.com>
2024-08-06 15:04:19 +0800

    index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.xmserver.xyz": dockerHub,
+  "quay.xmserver.xyz": "https://quay.io",
+  "gcr.xmserver.xyz": "https://gcr.io",
+  "k8s-gcr.xmserver.xyz": "https://k8s.gcr.io",
+  "k8s.xmserver.xyz": "https://registry.k8s.io",
+  "ghcr.xmserver.xyz": "https://ghcr.io",
+  "cloudsmith.xmserver.xyz": "https://docker.cloudsmith.io",
+  "ecr.xmserver.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.xmserver.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
